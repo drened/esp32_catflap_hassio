9e52969102d5555a77ecfa4c4068838e5e28e692
drened <94844873+drened@users.noreply.github.com>
2026-03-12 11:23:43 +0100

    Add config flow for CatFlap integration

--- homeassistant/custom_components/catflap/config_flow.py
+++ homeassistant/custom_components/catflap/config_flow.py
@@ -1,1 +1,27 @@
+import voluptuous as vol
 
+from homeassistant import config_entries
+
+from .const import DOMAIN, CONF_NAME
+
+
+class CatFlapConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+
+        if user_input is not None:
+            return self.async_create_entry(
+                title=user_input[CONF_NAME],
+                data=user_input
+            )
+
+        schema = vol.Schema({
+            vol.Required(CONF_NAME): str,
+        })
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema
+        )
