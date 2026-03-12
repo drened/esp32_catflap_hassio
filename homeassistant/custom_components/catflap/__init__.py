f0a56aee6a2b174763499e4ebfea666ae8038992
drened <94844873+drened@users.noreply.github.com>
2026-03-12 11:23:30 +0100

    Implement async setup and unload for catflap component

--- homeassistant/custom_components/catflap/__init__.py
+++ homeassistant/custom_components/catflap/__init__.py
@@ -1,1 +1,26 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
+from .const import DOMAIN
+
+PLATFORMS = ["binary_sensor", "sensor"]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {
+        "cats": {}
+    }
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+
+    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+    hass.data[DOMAIN].pop(entry.entry_id)
+
+    return True
